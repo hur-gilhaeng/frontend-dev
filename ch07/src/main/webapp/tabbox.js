614f8971c19156d596bfbd15c12d0b4b7c1b7aca
hur-gilhaeng <hgh8401@gmail.com>
2020-04-10 21:54:08 +0900

    과제용 tabBox 준비

---
+++ ch07/src/main/webapp/tabbox.js
@@ -0,0 +1,23 @@
+// jQuery 버젼
+var TabBox = {
+	init: function(){
+		window.addEventListener('load', this._onWindowLoad);		
+	},	
+	_onWindowLoad: function(){
+		var divTabBox = document.getElementsByClassName('tab-box')[0];
+		var ulTabBox = divTabBox.childNodes[1];
+		var liTabs = ulTabBox.getElementsByTagName('li');
+
+		for(var i = 0; i < liTabs.length; i++){
+			liTabs[i].addEventListener('click', TabBox._onTabClicked);
+		}
+	},
+	_onTabClicked: function(){
+		//unselect
+		var liSelecetds = document.getElementsByClassName('selected');
+		(liSelecetds.length == 1) && (liSelecetds[0].className = '');		
+
+		// seclect
+		this.className = 'selected';
+	}	
+}
